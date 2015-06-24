db4de801cc39bc732032db5d0abf39ee971ab9ff
George Song <george@55minutes.com>
2015-06-24 09:43:00 -0700

    Skeleton project

---
+++ Gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var serve = require('browser-sync');
+var sync = require('run-sequence');
+var webpack = require('gulp-webpack');
+
+
+var paths = {
+  app: ['client/app/**/*.js']
+};
+
+gulp.task('build', function() {
+  return gulp.src('client/app/app.js')
+  .pipe(webpack(require('./webpack.config')))
+  .pipe(gulp.dest('client'));
+});
+
+gulp.task('serve', function() {
+  serve({
+    port: 3000,
+    open: false,
+    server: {
+      baseDir: 'client'
+    }
+  });
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.app, ['build', serve.reload]);
+});
+
+gulp.task('dev', function(done) {
+  sync('build', 'watch', 'serve', done);
+});
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: './client/app/app.js',
+  output: {
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {test: /\.js$/, loader: 'babel', excludes: [/\.node_modules/]}
+    ]
+  }
+};
